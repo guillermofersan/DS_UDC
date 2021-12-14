032da4d633320013b36a39d5f6c56a1ce34baf79
Guillermo Fernandez <guillermofersan@gmail.com>
2021-12-14 17:34:13 +0100

    prueba

--- DS-61-03-DA/src/e2/StrongdepTraverser.java
+++ DS-61-03-DA/src/e2/StrongdepTraverser.java
@@ -6,22 +6,12 @@ import java.util.List;
 
 public class StrongdepTraverser implements GraphTraverser {
 
-    List<Task> aux(HashMap<Task, List<Task>> g, Task t){
-
-        List<Task> aux = new ArrayList<>();
-        List<Task> dependants = g.get(t);
-
-
-        //recursividad
-
-        g.remove(t);
-        return aux;
-
-    }
-
     @Override
     public List<Task> traversegraph(Graph graph) {
 
+
+
+        /*
         HashMap<Task, List<Task>> g = graph.getGraph();
         List<Task> aux = new ArrayList<>();
         List<Task> total = new ArrayList<>();
@@ -41,8 +31,10 @@ public class StrongdepTraverser implements GraphTraverser {
             total.addAll(aux);
             aux.clear();
         }
+        */
+        return null;
 
-        return total;
+
 
     }
 }
--- DS-61-03-DA/src/e2/Taskplanner.java
+++ DS-61-03-DA/src/e2/Taskplanner.java
@@ -1,18 +1,32 @@
 package e2;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.io.*;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public class Taskplanner {
 
     Graph graph;
 
-    public Taskplanner(String filename) throws IOException {
+    public Taskplanner(List<String> depList) {
 
         graph = new Graph();
 
+        Task t1,t2;
+
+        for (String s: depList) {
+            t1 = new Task(s.charAt(0));
+            t2 = new Task(s.charAt(5));
+
+            if (!graph.isVertex(t1)) graph.addVertex(t1);
+            if (!graph.isVertex(t2)) graph.addVertex(t2);
+
+            graph.addDependency(t1,t2);
+        }
+
+        /*
         if (filename==null) throw new IllegalArgumentException();
 
         try(BufferedReader br = new BufferedReader(new FileReader(filename))) {
@@ -38,6 +52,7 @@ public class Taskplanner {
             //System.out.println(s);
 
         }
+         */
     }
 
     public Graph getGraph() {
@@ -46,16 +61,28 @@ public class Taskplanner {
 
     public static void main(String[] args) throws IOException {
 
-        Taskplanner t = new Taskplanner("tasks.txt");
+        String[] array = {"C -> A","C -> F","A -> B","A -> D","B -> E","D -> E","F -> E","G -> F","G -> H","F -> J","H -> J"};
+
+        //Taskplanner t = new Taskplanner(Arrays.asList(array));
 
 
-        System.out.print(t.getGraph().toString() + "\n");
 
-        //if (t.getGraph().taskDepends(new Task('C')))
-        //    System.out.println("Buenas tardes");
+        HashMap<String, Integer> a = new HashMap<>();
+
+        a.put("B",5);
+        a.put("A",7);
+        a.put("D",2);
+        a.put("C",3);
+
+        System.out.println(a.toString());
 
 
-        System.out.println(t.getGraph().traverseGraph(new StrongdepTraverser()).toString());
+
+
+        //ystem.out.print(t.getGraph().toString() + "\n");
+
+
+        //System.out.println( t.getGraph().traverseGraph(new StrongdepTraverser()).toString() );
 
     }
 
